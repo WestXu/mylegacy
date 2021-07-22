1f8f11590972a63bd46888e8d527b1d17d9ecdfb
lxu <lxu@junzhiam.com>
2021-07-22 18:28:08 +0800

    reorg python files and add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="mylegacy",
+    version="0.0.1",
+    author="WestXu",
+    author_email="xu-lai-xi@live.com",
+    packages=setuptools.find_packages(),
+)
